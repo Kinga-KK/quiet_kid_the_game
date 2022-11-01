34ffcc837c3aadcf8a61827e461607a63c1d5c11
Kinga <kiss.kinga41@gmail.com>
2022-11-01 13:37:59 +0100

    added a bunch of text which are now randomly generated.

--- QuietKid_TheGame/QuietKid_TheGame/Game.cs
+++ QuietKid_TheGame/QuietKid_TheGame/Game.cs
@@ -9,6 +9,7 @@ namespace QuietKid_TheGame
     //todo: add option to save and load from local files
     class Game
     {
+        private Random r = new Random();
         private bool answerInvalid { get; set; }
         public bool playing { get; private set; }
         private Emil emil { get; set; }
@@ -32,13 +33,42 @@ namespace QuietKid_TheGame
         {
             days = new string[] 
             {
-                "Your first day of school. It passes without much action since you don't have any friends."
+                "Another day of school. It passes without much action.",
+                "You went to school and sat next to your friend. He's a quiet kid too. Sometimes you wonder if he thinks the same way as you, but you don't dare ask.",
+                "One of your teachers made a rude comment about you.",
+                "You got praised by your favourite teacher today.",
+                "You managed to score a good mark today.",
+                "You got a bad mark.",
+                "You heard a couple arguing in the hallways.",
+                "Some students were protesting in front of the school today.",
+                "All the teachers went on strike. You had a free day.",
+                "A student was playing a guitar in the breaks today.",
+                "You got told \"Buddy, I knew you were scamming before I even touched the bat, bats don't break from getting dropped from someone's hands. I said all that jargon just so I can flex all my knowledge about baseball.\" today.",
+                "Nothing.",
+                "You had to drag yourself to school and were thinking of going home all day.",
+                "You got humiliated in front of class. Again.",
+                "The only thing in your wallet today was despair.",
+                "You got disqualified on an exam because you had to go to the toilet.",
+                "You wasted all your money on snacks. It was worth it.",
+                "You got lost on the way to schooll today.",
+                "You \"got lost\" on the way to school today.",
+                "All your classes were online today. You were perfectly comfortable. Which means you weren't paying attention.",
+                "You almost died in school today.",
+                "You wake up and decide to become a cereal killer.",
+                "You wake up and decide to put milk before the cereal.",
+                "You pulled an all nighter to finish a group project, but found out you have a test today. It was your worst subject.",
+                "You slipped on a banana peel at 4:15 AM at Disneyland. You almost die because you're lactose intolerant.",
+                "Nobody helps you work on a group project and you all end up failing. They push the blame on you.",
+                "You eat cereal with edami cheese today.",
+                "You mix soggy cereal with crispy cereal.",
+                "No one knows but you were in the military for 3 months in Africa."
             };
         }
         private void setEndings()
         {
             endings = new string[]
             {
+                //todo: add more endings
                 "That was it. Yet another month. You guess not much changes during a month for Emil."
             };
         }
@@ -72,7 +102,7 @@ namespace QuietKid_TheGame
         public void NewDay()
         {
             Console.WriteLine($"\n> Day {day} <\n");
-            Console.WriteLine("> " + days[day - 1] + " <");
+            Console.WriteLine("> " + days[r.Next(0,days.Count())] + " <");
             BasicChoices();
             CheckEnd();
         }
@@ -102,6 +132,37 @@ namespace QuietKid_TheGame
                 }
             } while (answerInvalid);
         }
+        public void LifeChangingChoices()
+        {
+            Console.WriteLine("\n! Today you decide to do something different. Something life-changing. !");
+            Console.WriteLine("\n>>> What shall it be? <<<");
+            Console.WriteLine("T - Train" +
+                "\nC - Code" +
+                "\nS - Seethe in repressed rage");
+            do
+            {
+                Console.Write("\nWhat will you do? ");
+                switch (Console.ReadLine().ToLower())
+                {
+                    case "t":
+                    case "train":
+                        emil.Train(); answerInvalid = false;
+                        break;
+                    case "c":
+                    case "code":
+                        emil.Code(); answerInvalid = false;
+                        break;
+                    case "s":
+                    case "seethe":
+                    case "seethe in repressed rage":
+                        emil.Seethe(); answerInvalid = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid answer!"); answerInvalid = true;
+                        break;
+                }
+            } while (answerInvalid);
+        }
         public void CheckEnd()
         {
             if (day == days.Count())
@@ -127,6 +188,7 @@ namespace QuietKid_TheGame
         }
         public static void Goodbye()
         {
+            Console.WriteLine("\n\tCredits\n\n\tCode: K. Kinga\n\n\tMany thanks to Jyll, Jony and YoursTrulyTheReaper for the daily text ideas!");
             Console.WriteLine("\nGame ended. Exiting...");
         }
     }
