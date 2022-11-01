01edda78025d05200c15b1c8ea2ed3a02d4b5024
Kinga <kiss.kinga41@gmail.com>
2022-11-01 08:56:17 +0100

    made outer game loop

---
+++ QuietKid_TheGame/QuietKid_TheGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace QuietKid_TheGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool answerInvalid = false;
+            bool playing = false;
+            Console.WriteLine("Welcome to Quiet Kid: The Game!");
+            Console.WriteLine("\nEmil Edami is a quiet neumanner, living his life at the back of his class.\nWhat fate awaits him? Play to find out!\n");
+
+            do
+            {
+                Console.Write("Play game?(y/n): ");
+                string play = Console.ReadLine();
+                switch (play)
+                {
+                    case "y":
+                    case "yes": playing = true; answerInvalid = false;
+                        break;
+                    case "n":
+                    case "no": playing = false; answerInvalid = false;
+                        break;
+                    default: answerInvalid = true;
+                        break;
+                }
+            } while (answerInvalid);
+
+            while (playing)
+            {
+                Console.WriteLine("game will be here");
+                playing = false;
+            }
+
+            Console.WriteLine("\nGame ended. Exiting...");
+        }
+    }
+}
